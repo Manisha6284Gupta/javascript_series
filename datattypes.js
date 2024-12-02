b3345f513a2113935efacf8d98c99d71d176f317
Manisha Gupta <140302444+Manisha6284Gupta@users.noreply.github.com>
2024-12-02 14:48:37 +0530

    Create datattypes.js

---
+++ datattypes.js
@@ -0,0 +1,28 @@
+let score = 33
+// console.log(typeof score)
+// console.log(typeof (score))
+// const {score} = req.body
+
+// let score = "231A"
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+
+
+let isLoggedIn = 1 //"manisha"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "manisha" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
