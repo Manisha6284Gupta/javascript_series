c880fea88d21cb0fbb5e0a3f133c05669efc2225
Manisha Gupta <140302444+Manisha6284Gupta@users.noreply.github.com>
2024-12-02 14:49:44 +0530

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 == 1)
+
+//console.log("2" > 1);
+// console.log("02" > 1);
+
+ 
+// console.log(null == 0);
+// console.log(null > 0)
+// console.log(null >= 0)
+// Avoid this type of comparision
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+//*************Tripple Equal***********/
+//it also check datatype
+
+console.log("2" === 2)
+
