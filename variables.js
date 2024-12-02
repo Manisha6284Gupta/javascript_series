6c6a70a60c35c39ed8f5c724d03663f6ef310d41
Manisha Gupta <140302444+Manisha6284Gupta@users.noreply.github.com>
2024-12-02 14:47:53 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,7 @@
+const accountId = 13445
+let   accountName = "Manisha Gupta"
+var   accountCity = " Ludhiana"
+console.table([accountId,accountName,accountCity])
+/*
+Note : Don't use var keyword due to its fuctional and block scope.
+*/
